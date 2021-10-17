763005a0f38ba951fa5924244acc806325f937b9
Gabriel Mackie <gabrielmackie@hotmail.com>
2021-10-17 13:43:30 +0100

    The page_finder code can now scrape an entire page Need to add multi-page scraping The test product output mostly matched what I could see. Can't figure out the cause of the discrepancies.

--- page_finder.py
+++ page_finder.py
@@ -24,52 +24,71 @@ headers = ({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Ge
 
 
 # TODO: Once this fully works, try messing around with how short I can get the search term
-# Create an amazon link from the search term 
+# Create an amazon link from the search term
 def build_amz_search_url(brand_string, page_no):
-    
+
     # Replace the spaces in the search term with plus symbols
-    search_term = brand_string.replace(' ','+')
-    
+    search_term = brand_string.replace(' ', '+')
+
     # And format them into an amazon url
-    url = (f'https://www.amazon.co.uk/s?k={search_term}&i=toys&rh=p_89%3A{search_term}&dc&page={page_no}&crid=1SDTYPYXM70YV&qid=1561713015&rnid=1632651031&sprefix=my+li%2Ctoys%2C163&ref=sr_pg_{page_no}')
+    url = (
+        f'https://www.amazon.co.uk/s?k={search_term}&i=toys&rh=p_89%3A{search_term}&dc&page={page_no}&crid=1SDTYPYXM70YV&qid=1561713015&rnid=1632651031&sprefix=my+li%2Ctoys%2C163&ref=sr_pg_{page_no}')
     return url
 
 # Extract and return the information from a product
+
+
 def extract_product(item):
-    
+
     # Description and url
     atag = item.h2.a
     description = atag.text.strip()
     url = 'https://www.amazon.co.uk' + atag.get('href')
-    
+
     # Price
     # An item has no price if it's currently unavailable
-    try: 
+    try:
         price_parent = item.find('span', {'class': 'a-price'})
-        price = float(price_parent.find('span', {'class': 'a-offscreen'}).text.replace('£', ''))
+        price = float(price_parent.find(
+            'span', {'class': 'a-offscreen'}).text.replace('£', ''))
     except AttributeError:
         price = "NA"
-    
+
     # Rating and number of reviews
     # Some items have no ratings or reviews, if so we want to mark the reviews as 0 and rating as NA
     try:
         rating = item.i.text.replace('out of 5 stars', '').strip()
-        review_count = int(item.find('span', {'class': 'a-size-base'}).text.replace(',', ''))
+        review_count = int(
+            item.find('span', {'class': 'a-size-base'}).text.replace(',', ''))
     except AttributeError:
         rating = "NA"
         review_count = 0
     except ValueError:
+        '''
+        # Value errors are thrown up because there are multiple things with the class a-size-base in the item
+        # Only one of these is the actual number of ratings.
+        # I could run this code and then extract the 2nd element, which is the number of ratings
+        # However, the original code seems to work almost every time. This code at the stage it's currently at
+        # creates more errors. Not totally sure why the original code works most of the time.
+        # I'm going to just leave it, as something that can be changed if the errors are too big of a problem
+        review_count = item.find_all(
+            'span', {'class': 'a-size-base'})
+        print(review_count)
+        print(type(review_count))'''
         review_count = "Error"
-    
+
     result = (description, url, price, rating, review_count)
 
     return result
+
 
 # This generates a url using our function
 url = build_amz_search_url(brand_string, 1)
 
 # This calls the actual page (currently using a custom url)
-r = requests.get(url = 'https://www.amazon.co.uk/s?k=lego&qid=1633700647&ref=sr_pg_1', headers = headers, timeout = 1)
+r = requests.get(
+    url=url, headers=headers, timeout=1)
+# Test url for product_urls_changed.xlsx is 'https://www.amazon.co.uk/s?k=lego&qid=1633700647&ref=sr_pg_1'
 
 # Create a soup object from the webpage
 soup = BeautifulSoup(r.text, 'lxml')
@@ -85,8 +104,9 @@ for item in results:
     product = extract_product(item)
     products.append(product)
 
-print(products[0])
+print(url)
 
 # Convert our list to a pandas df and save it to an excel file
 df = pd.DataFrame(products)
+df.columns = ['name', 'url', 'price', 'rating', 'num_of_ratings']
 df.to_excel('product_urls.xlsx', index=False)
